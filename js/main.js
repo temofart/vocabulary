4e64b58cd3a1f3be0c5723e8d941381944dd8ff2
Artyom O <18art12@gmail.com>
2018-06-28 16:01:25 +0300

    all to obj

--- js/main.js
+++ js/main.js
@@ -8,36 +8,7 @@ window.onload = function main() {
 		word: [],
 		translate: [],
 		description: []
-	}
-
-	function createElement() {
-		var newItem = document.createElement('div');
-		newItem.classList="item";
-		newItem.id="item";
-		allItem.appendChild(newItem);
-
-		var closeDiv = document.createElement('div');
-		closeDiv.id = "close";
-		closeDiv.classList = "close";
-		closeDiv.onclick = removeItem;
-
-		var firstDiv = document.createElement('div');
-		firstDiv.id = "firstWord";
-		firstDiv.classList = "word first-word";
-
-		var secondDiv = document.createElement('div');
-		secondDiv.id = "secondWord";
-		secondDiv.classList = "word second-word";
-
-		var descrDiv = document.createElement('div');
-		descrDiv.id = "description";
-		descrDiv.classList = "word description";
-
-		newItem.appendChild(closeDiv);
-		newItem.appendChild(firstDiv);
-		newItem.appendChild(secondDiv);
-		newItem.appendChild(descrDiv);
-	}
+  }
 
 	var makeVocab = {
 		getValues: function() {
@@ -55,8 +26,35 @@ window.onload = function main() {
 			vocab.translate.push(this.translateValue);
 			vocab.description.push(this.descriptionValue);
 		},
+    createElements: function() {
+      var newItem = document.createElement('div');
+      newItem.classList="item";
+      newItem.id="item";
+      allItem.appendChild(newItem);
+  
+      var closeDiv = document.createElement('div');
+      closeDiv.id = "close";
+      closeDiv.classList = "close";
+      closeDiv.onclick = removeItem;
+  
+      var firstDiv = document.createElement('div');
+      firstDiv.id = "firstWord";
+      firstDiv.classList = "word first-word";
+  
+      var secondDiv = document.createElement('div');
+      secondDiv.id = "secondWord";
+      secondDiv.classList = "word second-word";
+  
+      var descrDiv = document.createElement('div');
+      descrDiv.id = "description";
+      descrDiv.classList = "word description";
+  
+      newItem.appendChild(closeDiv);
+      newItem.appendChild(firstDiv);
+      newItem.appendChild(secondDiv);
+      newItem.appendChild(descrDiv);
+    },
 		pushElements: function() {
-			createElement();
 			var item = document.getElementById('all-words').lastChild;
 			var lastWord = vocab.word.slice(-1)[0];
 			var lastTranslate = vocab.translate.slice(-1)[0];
@@ -65,13 +63,17 @@ window.onload = function main() {
 			item.getElementsByClassName("word")[0].innerHTML = lastWord;
 			item.getElementsByClassName("word")[1].innerHTML = lastTranslate;
 			item.getElementsByClassName("word")[2].innerHTML = lastDescr;
-		}
+    },
+    saveLocal: function() {return localStorage.setItem('vocab', JSON.stringify(vocab));}
+
 	};
 	btnAdd.addEventListener('click', function() {
 		makeVocab.getValues();
 		makeVocab.resetInput();
 		makeVocab.pushWords();
-		makeVocab.pushElements();
+		makeVocab.createElements();
+    makeVocab.pushElements();
+    makeVocab.saveLocal();
 	});
 
 	function removeItem() {
